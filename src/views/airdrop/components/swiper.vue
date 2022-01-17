cc619c0609d88d9a2c5ccfb0bcf85cb777d6bd56
wj <intouue@gmail.com>
2022-01-17 18:20:43 +0800

    'airdrop页面的移动端响应式布局，轮播效果'

--- src/views/airdrop/components/swiper.vue
+++ src/views/airdrop/components/swiper.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>111</div>
+  <div class="subscribes-swiper">
+11111111
+
+    <!-- <div v-for="(item, index) in subscribes" :key="index">
+        <a :href="item.link" target="view_window">
+            <img :src="item.img" alt="" />
+        </a>
+        <p v-if="item.state">Subscribed</p>
+        <p v-else>check subscribe</p>
+    </div> -->
+
+  </div>
 </template>
 
 <script setup lang="ts">
-import { defineComponent, ref, Prop } from 'vue';
+
+
+import { setgroups } from 'process';
+import { onMounted,defineComponent, ref, Prop, mergeProps } from 'vue';
+const props = defineProps({
+  subscribes:Array
+})
+console.log(props.subscribes);
+
+
+
 
 // export default defineComponent({
 //     name: 'Tabs',
@@ -31,6 +52,42 @@ import { defineComponent, ref, Prop } from 'vue';
 
 </script>
 
-<style>
+<style lang="less" scoped>
+.subscribes-swiper{
+  width: 100%;
+  height: 150px;
+  background: #ccc;
+  .swiper{
+    width: 100%;
+    height: 100%;
+    .swiper-slide{
+      width: 100%;
+    height: 100%;
+    }
+  }
+//             display: flex;
+//             flex-direction: row;
+//            div{
+//               width: 30vw;
+//               text-align: center;
+//  border: 1px solid;
+//                 padding: 1vw 0;
+//                 border-radius: 1vw;
+//                 p{
+//                     margin: 1vw 0;
+//                 }
+//                 img{
+//                     width: 40%;
+//                 }
 
+//               // display: flex;
+//               // justify-items: center;
+//            }
+//            & > div:not(:last-child){
+//                 margin-right: 2%;
+//             }
+//             & > div:hover{
+//                 transform: scale(1.2);
+//             }
+        }
 </style>
